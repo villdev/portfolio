5f3139a7ae4f14ed1940caaa939010b2735a6c52
vill <villdevelop@gmail.com>
2020-11-30 22:54:37 +0530

    added initial loading anim

---
+++ js/main.js
@@ -0,0 +1,31 @@
+function start() {
+  const t1 = gsap.timeline();
+
+  t1.to(
+    ".logo__vill",
+    { duration: 0.7, x: 240, scale: 0.99, ease: "expo.inOut" },
+    "start1"
+  )
+    .to(".logo__dev", { duration: 0.7, x: 0, ease: "expo.inOut" }, "start1")
+    .to(
+      ".logo__vill",
+      { duration: 1, x: -240, ease: "expo.inOut", delay: 1 },
+      "start2"
+    )
+    .to(
+      ".logo__dev",
+      { duration: 1, x: -560, ease: "expo.inOut", scale: 0.7, delay: 1 },
+      "start2"
+    )
+    .to(
+      ".logo__vill",
+      { duration: 0.7, x: 0, ease: "back.inOut(3)", delay: 1 },
+      "start3"
+    )
+    .to(
+      ".logo__dev",
+      { duration: 0.7, x: -240, scale: 1, ease: "back.inOut(3)", delay: 1 },
+      "start3"
+    );
+}
+document.body.addEventListener("click", start);
